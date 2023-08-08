2ce933166d9fdae4e2a56894b25b7183bbffa1dc
Todor Borisov <todor0203@gmail.com>
2023-08-08 19:55:45 +0300

    Update index.js

--- Rest-Server/index.js
+++ Rest-Server/index.js
@@ -27,8 +27,6 @@ dbConnector()
   })
   .catch(console.error);
 
-
-// Set up a middleware to handle CORS
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', config.origin);
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
